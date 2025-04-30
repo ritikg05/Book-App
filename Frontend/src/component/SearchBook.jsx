bb9e0438f2dd8c7cc534b6fbf2fedfbf1e0ea4ad
ritikg05 <hrg19976@gmail.com>
2025-04-30 12:14:47 +0530

    frontend

--- Frontend/src/component/SearchBook.jsx
+++ Frontend/src/component/SearchBook.jsx
@@ -13,7 +13,7 @@ const SearchBook = () => {
     }
 
     try {
-      const res = await axios.get(`http://localhost:9000/search?title=${query}`);
+      const res = await axios.get(`https://book-app-n2lc.onrender.com/search?title=${query}`);
       setBooks(res.data);
     } catch (error) {
       console.error(error);
--- Frontend/src/component/ViewBook.jsx
+++ Frontend/src/component/ViewBook.jsx
@@ -11,7 +11,7 @@ const ViewBook = () => {
 
   const handleView = async () => {
     try {
-      const res = await axios.get('http://localhost:9000/books');
+      const res = await axios.get('https://book-app-n2lc.onrender.com/books');
       setBooks(res.data);
     } catch (err) {
       console.log(err);
