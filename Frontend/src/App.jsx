dd54f2741d33add6aae4926f6679219c5aa12866
ritikg05 <hrg19976@gmail.com>
2025-04-30 11:16:33 +0530

    Normalize line endings

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import AddBook from './component/AddBook'
+import DeleteBook from './component/DeleteBook'
+import SearchBook from './component/SearchBook'
+import UpdateBook from './component/UpdateBook'
+import ViewBook from './component/ViewBook'
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <Router>
+        <nav>
+          <Link to="/add">Add Book</Link>
+          <Link to="/view">View Book</Link>
+          <Link to="/search">Search Book</Link>
+          <Link to="/update">Update Book</Link>
+          <Link to="/delete">Delete Book</Link>
+        </nav>
+        <Routes>
+          <Route path="/add" element={<AddBook />} />
+          <Route path="/delete" element={<DeleteBook />} />
+          <Route path="/search" element={<SearchBook />} />
+          <Route path="/update" element={<UpdateBook />} />
+          <Route path="/view" element={<ViewBook />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
